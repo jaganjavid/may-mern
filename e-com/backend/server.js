4ac5842a768bdc67ecc177b530f62b7eb02d7ef8
jaganjavid <jagantutor1@gmail.com>
2024-10-01 21:47:57 +0530

    Added a middleware

--- e-com/backend/server.js
+++ e-com/backend/server.js
@@ -1,30 +1,44 @@
 
 import express from "express";
-import products from "./data/products.js";
+
 import cors from "cors"
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js"
 
 
 const app = express();
+
+
 app.use(cors());
 
 connectDB();
 
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world !");
+app.get("/", (req, res, next) => {
+    console.log("Home");
+    res.send("Home page");
+    next();
 })
 
-app.get("/api/products", (req, res) => {
-    
-    res.json(products);
+app.get("/users", auth, (req, res) => {
+    console.log("user page")
+    res.send("users page");
 })
 
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+
+function auth(req, res, next){
+    console.log("Auth");
+    next();
+}
+
+function log(req, res, next){
+    console.log("LOG");
+    next()
+}
+
+app.use("/api/products", productRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
